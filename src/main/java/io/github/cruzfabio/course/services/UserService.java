3bcd51e94fd33848463eec16d1d39f6794e0ad4d
CruzFabio <cruz.fabiocorrea@gmail.com>
2026-01-31 02:04:58 -0300

    Service layer, component registration

---
+++ src/main/java/io/github/cruzfabio/course/services/UserService.java
@@ -0,0 +1,26 @@
+package io.github.cruzfabio.course.services;
+
+import io.github.cruzfabio.course.entities.User;
+import io.github.cruzfabio.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
