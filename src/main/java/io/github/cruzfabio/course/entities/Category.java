0b51c417e8d9b8ae2f46f7d389d92cd2baeb8287
CruzFabio <cruz.fabiocorrea@gmail.com>
2026-02-05 02:09:45 -0300

    Many-to-many association with JoinTable

--- src/main/java/io/github/cruzfabio/course/entities/Category.java
+++ src/main/java/io/github/cruzfabio/course/entities/Category.java
@@ -1,5 +1,6 @@
 package io.github.cruzfabio.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,7 +15,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany (mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {}
@@ -55,6 +57,5 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
-
 
 }
