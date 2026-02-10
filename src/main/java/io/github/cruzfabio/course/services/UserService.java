c28a256b3cdc5d5da6bd146df2b1cf61c2cd67cc
CruzFabio <cruz.fabiocorrea@gmail.com>
2026-02-09 23:45:42 -0300

    Exception handling - Delete

--- src/main/java/io/github/cruzfabio/course/services/UserService.java
+++ src/main/java/io/github/cruzfabio/course/services/UserService.java
@@ -2,8 +2,11 @@ package io.github.cruzfabio.course.services;
 
 import io.github.cruzfabio.course.entities.User;
 import io.github.cruzfabio.course.repositories.UserRepository;
+import io.github.cruzfabio.course.services.exceptions.DatabaseException;
 import io.github.cruzfabio.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
